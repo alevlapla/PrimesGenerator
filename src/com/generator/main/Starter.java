99cbd280646f0dac65c531063c128c6d32671fdb
bratishka <alevlapla@gmail.com>
2022-11-27 20:01:36 +0300

    A little less more rough implementation of a prime number generator.

--- src/com/generator/main/Starter.java
+++ src/com/generator/main/Starter.java
@@ -8,7 +8,7 @@ public class Starter {
 
 	public static void main(String[] args) throws InterruptedException {
 //		Thread pg_int = new Thread(new PrimeGeneratorIntBased(), "generator");
-		Thread pg_int = new Thread(new PrimeGeneratorArrayBased(), "generator");
+		Thread pg_int = new Thread(new PrimeGeneratorArrayBasedOptimized(), "generator");
 		pg_int.start();
 
 		try {
