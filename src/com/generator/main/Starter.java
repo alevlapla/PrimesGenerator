3b9fed0491d744098d1f7c947370aed8b56ab038
bratishka <alevlapla@gmail.com>
2022-11-28 22:36:38 +0300

    Perfect HashMap-based prime numbers generator.

--- src/com/generator/main/Starter.java
+++ src/com/generator/main/Starter.java
@@ -7,7 +7,7 @@ public class Starter {
 	final static Logger logger = LoggerFactory.getLogger(Starter.class);
 
 	public static void main(String[] args) throws InterruptedException {
-		Thread pg_int = new Thread(new PrimeGeneratorIntBased(), "generator");
+		Thread pg_int = new Thread(new PrimeGeneratorHashMapBased(), "generator");
 		pg_int.start();
 
 		try {
